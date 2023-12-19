4a24c5cc4486f53ecabb3b71ae1a3be6d882fa5d
ash-india <niteshmodi00007@gmail.com>
2023-12-19 20:25:06 +0530

    rectified the request link

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -15,7 +15,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`https://mern-bookstore-backend-nitesh-modis-projects.vercel.app/${id}`)
+      .delete(`https://mern-bookstore-backend-nitesh-modis-projects.vercel.app/books/${id}`)
       .then(()=>{
         setLoading(false);
         enqueueSnackbar('Deleted Successfully' , {variant : 'success'});
